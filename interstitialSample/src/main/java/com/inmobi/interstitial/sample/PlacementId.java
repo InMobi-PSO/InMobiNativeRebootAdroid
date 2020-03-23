0040d8a703c4889c86c73506ad0aec8dd22bd998
Inmobi-Hackett <hackett.zhang@inmobi.com>
2020-03-23 11:50:00 +0800

    update demo 7.4.2

--- interstitialSample/src/main/java/com/inmobi/interstitial/sample/PlacementId.java
+++ interstitialSample/src/main/java/com/inmobi/interstitial/sample/PlacementId.java
@@ -3,8 +3,8 @@ package com.inmobi.interstitial.sample;
 public interface PlacementId {
     // PLID for Interstitial full screen
 //    long YOUR_PLACEMENT_ID = 1476878485960L;
-//    long YOUR_PLACEMENT_ID = 1498491299441L;
-    long YOUR_PLACEMENT_ID = 1542549638032L;
+    long YOUR_PLACEMENT_ID = 1498491299441L;
+//    long YOUR_PLACEMENT_ID = 1542549638032L;
 
 
     // PLID for Rewarded video
